6ebcd8795877d95d8ef941944215846e25db360a
Micah Wood <micah@wpscholar.com>
2021-10-13 06:10:10 -0400

    Add email to the list of things sent to the Hiive (#19)

--- src/HubConnection.php
+++ src/HubConnection.php
@@ -267,14 +267,15 @@ class HubConnection implements SubscriberInterface {
 		global $wpdb, $wp_version;
 
 		return array(
-			'url'         => get_site_url(),
-			'php'         => phpversion(),
-			'mysql'       => $wpdb->db_version(),
-			'wp'          => $wp_version,
-			'plugin'      => BLUEHOST_PLUGIN_VERSION,
-			'hostname'    => gethostname(),
 			'cache_level' => intval( get_option( 'endurance_cache_level', 2 ) ),
 			'cloudflare'  => get_option( 'endurance_cloudflare_enabled', false ),
+			'email'       => get_option( 'admin_email' ),
+			'hostname'    => gethostname(),
+			'mysql'       => $wpdb->db_version(),
+			'php'         => phpversion(),
+			'plugin'      => BLUEHOST_PLUGIN_VERSION,
+			'url'         => get_site_url(),
+			'wp'          => $wp_version,
 		);
 
 	}
