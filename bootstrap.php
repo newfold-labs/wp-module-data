d00db8bd6aaa40256cd4894e9820ed2044569671
Micah Wood <micah@wpscholar.com>
2023-07-18 17:23:58 -0400

    Bump version to 2.4.1

--- bootstrap.php
+++ bootstrap.php
@@ -19,7 +19,7 @@ if ( defined( 'NFD_DATA_MODULE_VERSION' ) ) {
 	exit;
 }
 
-define( 'NFD_DATA_MODULE_VERSION', '2.4.0' );
+define( 'NFD_DATA_MODULE_VERSION', '2.4.1' );
 
 if ( function_exists( 'is_admin' ) && is_admin() ) {
 	$upgrade_handler = new UpgradeHandler(
