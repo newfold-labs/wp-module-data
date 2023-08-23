4d1995eef24b34fbd308226169ac7cbe1040c42f
sumathi3399 <sumathivarma3399@gmail.com>
2023-08-23 12:29:02 +0530

    PRESS4-300 | event tracking for hive analytics

--- src/Listeners/Yith.php
+++ src/Listeners/Yith.php
@@ -15,6 +15,10 @@ class Yith extends Listener {
 	public function register_hooks() {
 		// Paypal Connection
 		add_filter( 'pre_update_option_yith_ppwc_merchant_data_production', array( $this, 'paypal_connection' ), 10, 2 );
+		add_filter( 'pre_update_option_nfd_ecommerce_captive_flow_razorpay', array( $this, 'razorpay_connection' ), 10, 2 );
+		add_filter( 'pre_update_option_nfd_ecommerce_captive_flow_shippo', array( $this, 'shippo_connection' ), 10, 2 );
+
+		add_filter( 'registered_post_type_yith_campaign', array( $this, 'campaign_created' ));
 	}
 
 	/**
@@ -26,12 +30,80 @@ class Yith extends Listener {
 	 * @return string The new option value
 	 */
 	public function paypal_connection( $new_option, $old_option ) {
-		if ( $new_option !== $old_option && ! empty( $new_option ) ) {
+		$url =  isset($_SERVER['HTTPS']) &&
+		$_SERVER['HTTPS'] === 'on' ? "https://" : "http://"; 
+		$url .= $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+		$data = array(
+			"action" 	=> "payment_connected", 
+			"category" 	=> "commerce", 
+			"data" 		=> array( 
+				"label_key" => "provider",
+				"provider" 	=> "yith_paypal",
+				"page" 		=> $url
+			), 
+		);
+		if ( $new_option !== $old_option && ! empty( $new_option ) ) {	
 			$this->push(
-				'yith_payment_connected',
-				array(
-					'provider' => 'paypal',
-				)
+				$data
+			);
+		}
+
+		return $new_option;
+	}
+
+	/**
+	 * Razorpay connected
+	 *
+	 * @param string $new_option New value of the razorpay_data_production option
+	 * @param string $old_option Old value of the razorpay_data_production option
+	 *
+	 * @return string The new option value
+	 */
+	public function razorpay_connection( $new_option, $old_option ) {
+		$url =  isset($_SERVER['HTTPS']) &&
+		$_SERVER['HTTPS'] === 'on' ? "https://" : "http://"; 
+		$url .= $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+		$data = array(
+			"action" 	=> "payment_connected", 
+			"category" 	=> "commerce", 
+			"data" 		=> array( 
+				"label_key" => "provider",
+				"provider" 	=> "razorpay",
+				"page" 		=> $url
+			), 
+		);
+		if ( $new_option !== $old_option && ! empty( $new_option ) ) {	
+			$this->push(
+				$data
+			);
+		}
+
+		return $new_option;
+	}
+
+	/**
+	 * Shippo connected
+	 *
+	 * @param string $new_option New value of the shippo_data option
+	 * @param string $old_option Old value of the shippo_data option
+	 *
+	 * @return string The new option value
+	 */
+	public function shippo_connection( $new_option, $old_option ) {
+		$url =  isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https://" : "http://"; 
+		$url .= $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+		$data = array(
+			"action" 	=> "shpping_connected", 
+			"category" 	=> "commerce", 
+			"data" 		=> array( 
+				"label_key" => "provider",
+				"provider" 	=> "shippo",
+				"page"		=> $url
+			), 
+		);
+		if ( $new_option !== $old_option && ! empty( $new_option ) ) {	
+			$this->push(
+				$data
 			);
 		}
 
