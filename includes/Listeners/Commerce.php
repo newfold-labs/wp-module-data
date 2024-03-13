2fd8f86e5ed43568355ea1e584839643f3ff45e2
Evan Mullins <evanrm@gmail.com>
2024-03-13 12:44:35 -0400

    some lint fixes

--- includes/Listeners/Commerce.php
+++ includes/Listeners/Commerce.php
@@ -60,7 +60,7 @@ class Commerce extends Listener {
 		if ( ! isset( $data['meta'] ) ) {
 			$data['meta'] = array();
 		}
-		if(isset(wp_count_posts( 'product' )->publish)){
+		if( isset( wp_count_posts( 'product' )->publish ) ) {
 			$data['meta']['products_count'] = (int) wp_count_posts( 'product' )->publish;
 		}
 
@@ -78,7 +78,7 @@ class Commerce extends Listener {
 		if ( ! isset( $data['meta'] ) ) {
 			$data['meta'] = array();
 		}
-		if(isset(wp_count_posts( 'shop_order' )->publish)){
+		if( isset( wp_count_posts( 'shop_order' )->publish ) ) {
 			$data['meta']['orders_count'] = (int) wp_count_posts( 'shop_order' )->publish;
 		}
 
