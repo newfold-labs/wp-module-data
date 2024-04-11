857f9fed94069acb3302b45198e86c938cd826a6
manikantakailasa <manikanta.kailasa@gmail.com>
2024-04-11 12:39:30 +0530

    moving code to 2.4.22

--- bootstrap.php
+++ bootstrap.php
@@ -139,29 +139,6 @@ SQL;
 }
 
 /**
- * Update affiliation code in option table
- *
- * @param object $container Container information
- */
-function nfd_update_options_table( $container ) {
-	$brand_code = array(
-		'bluehost'        => '86241',
-		'hostgator'       => '57686',
-		'web'             => '86239',
-		'crazy-domains'   => '57687',
-		'default'         => '86240',
-	);
-	$brand      = $container->plugin()->id;
-	if ( is_plugin_active( 'jetpack/jetpack.php' ) ) {
-		if ( empty( $brand ) || ! array_key_exists( $brand, $brand_code ) ) {
-				$brand = 'default';
-		}
-		$jetpack_affiliate_code = get_option( 'jetpack_affiliate_code' );
-		! $jetpack_affiliate_code && update_option( 'jetpack_affiliate_code', $brand_code[ $brand ] );
-	}
-}
-
-/**
  * Drop the event queue table
  */
 function nfd_drop_event_queue_table() {
