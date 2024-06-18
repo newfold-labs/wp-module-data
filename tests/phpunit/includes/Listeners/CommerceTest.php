6ded638cb4e8b60c7343f737bc15d4b6a6cc6525
Brian Henry <BrianHenryIE@gmail.com>
2024-06-18 10:13:27 -0700

    Add test for `Commerce::woocommerce_hpos_enabled()`

---
+++ tests/phpunit/includes/Listeners/CommerceTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace NewfoldLabs\WP\Module\Data\Listeners;
+
+use Mockery;
+use NewfoldLabs\WP\Module\Data\Event;
+use NewfoldLabs\WP\Module\Data\EventManager;
+use WP_Mock;
+use WP_User;
+
+/**
+ * @coversDefaultClass \NewfoldLabs\WP\Module\Data\Listeners\Commerce
+ */
+class CommerceTest extends \WP_Mock\Tools\TestCase {
+
+	public function setUp(): void {
+		parent::setUp();
+		WP_Mock::setUp();
+	}
+
+	public function tearDown(): void {
+		parent::tearDown();
+		WP_Mock::tearDown();
+
+		unset( $_SERVER['HTTP_HOST'] );
+		unset( $_SERVER['REQUEST_URI'] );
+		unset( $_SERVER['REMOTE_ADDR'] );
+	}
+
+	/**
+	 * @covers ::woocommerce_hpos_enabled
+	 */
+	public function test_woocommerce_hpos_enabled(): void {
+
+		$event_manager = Mockery::mock( EventManager::class );
+		$event_manager->expects( 'push' )->once()
+			->withArgs(
+				function ( Event $event ) {
+					return $event->data['label_key'] === 'type' && $event->data['type'] === 'hpos';
+				}
+			);
+
+		$sut = new Commerce( $event_manager );
+
+		WP_Mock::userFunction( 'is_ssl' )->once()->andReturnTrue();
+		$_SERVER['HTTP_HOST']   = '';
+		$_SERVER['REQUEST_URI'] = '';
+
+		/**
+		 * @see Event::__construct
+		 */
+		$_SERVER['REMOTE_ADDR'] = '';
+		$wp_user                = Mockery::mock( WP_User::class )->makePartial();
+		$wp_user->data          = new \stdClass();
+		$wp_user->user_nicename = 'admin';
+		WP_Mock::userFunction( 'get_current_user_id' )->twice()->andReturn( 1 );
+		WP_Mock::userFunction( 'get_user_by' )->once()->andReturn( $wp_user );
+		WP_Mock::userFunction( 'get_user_locale' )->once()->andReturn( 'en_US' );
+
+		$sut->woocommerce_hpos_enabled( '', 'yes', 'woocommerce_custom_orders_table_enabled' );
+
+		$this->expectNotToPerformAssertions();
+	}
+}
