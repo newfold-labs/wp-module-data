7b4562d6c82839d899fd76f24b8c2439071b0d7e
Brian Henry <BrianHenryIE@gmail.com>
2024-06-19 11:27:38 -0700

    Delete rough-work test

--- tests/phpunit/includes/EventManagerTest.php
+++ tests/phpunit/includes/EventManagerTest.php
@@ -169,21 +169,5 @@ class EventManagerTest extends \WP_Mock\Tools\TestCase {
 	}
 
 
-	/**
-	 * @covers ::send
-	 */
-	public function test_send(): void {
-
-		$sut = new EventManager();
-
-		$event_mock = Mockery::mock(Event::class);
-
-		$subscriber_mock = Mockery::mock(SubscriberInterface::class)->makePartial();
-
-		$sut->add_subscriber($subscriber_mock);
-
-		$sut->send( array( $event_mock ) );
-
-	}
-
+	
 }
