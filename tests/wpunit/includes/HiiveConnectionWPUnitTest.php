eb13b4639bb907c01b0b67ed27a515961862bc95
Brian Henry <BrianHenryIE@gmail.com>
2024-06-20 15:35:15 -0700

    Fix: `HiiveConnection::notify()` when `WP_Error` returned in request

---
+++ tests/wpunit/includes/HiiveConnectionWPUnitTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace NewfoldLabs\WP\Module\Data;
+
+use Mockery;
+use function NewfoldLabs\WP\ModuleLoader\container;
+
+/**
+ * @coversDefaultClass \NewfoldLabs\WP\Module\Data\HiiveConnection
+ */
+class HiiveConnectionWPUnitTest extends \lucatume\WPBrowser\TestCase\WPTestCase {
+
+	/**
+	 * @covers ::notify
+	 */
+	public function test_notify_wp_error(): void {
+
+		$sut = new HiiveConnection();
+
+		$event = Mockery::mock( Event::class );
+
+		update_option( 'nfd_data_token', 'appear-connected' );
+
+		$container = new \NewfoldLabs\WP\ModuleLoader\Container();
+		container( $container );
+
+		$plugin = Mockery::mock( \NewfoldLabs\WP\ModuleLoader\Plugin::class );
+		$plugin->shouldReceive( 'get' )->andReturnUsing(
+			function () {
+				$args = func_get_args();
+				return $args[1] ?? $args[0];
+			}
+		);
+		$container->set( 'plugin', $plugin );
+
+		/**
+		 * @see WP_Http::request()
+		 */
+		add_filter(
+			'pre_http_request',
+			function () {
+				return new \WP_Error( 'http_request_not_executed', 'User has blocked requests through HTTP.' );
+			}
+		);
+
+		try {
+			$sut->notify( array( $event ), false );
+		} catch ( \Error $error ) {
+			$this->fail( $error->getMessage() . PHP_EOL . $error->getTraceAsString() );
+		}
+
+		$this->assertTrue( true );
+	}
+}
