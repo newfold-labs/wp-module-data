dd5fdaf26775b9f5d7d96fe43e976614fbd9a273
Brian Henry <BrianHenryIE@gmail.com>
2024-06-20 15:46:10 -0700

    Add test for `HiiveConnection::notify()` non-blocking behavior

--- tests/wpunit/includes/HiiveConnectionWPUnitTest.php
+++ tests/wpunit/includes/HiiveConnectionWPUnitTest.php
@@ -13,6 +13,53 @@ class HiiveConnectionWPUnitTest extends \lucatume\WPBrowser\TestCase\WPTestCase 
 	/**
 	 * @covers ::notify
 	 */
+	public function test_notify_non_blocking(): void {
+
+		$sut = new HiiveConnection();
+
+		$event = Mockery::mock( Event::class );
+
+		update_option( 'nfd_data_token', 'appear-connected' );
+
+		$container = new \NewfoldLabs\WP\ModuleLoader\Container();
+		container( $container );
+
+		$plugin = Mockery::mock( \NewfoldLabs\WP\ModuleLoader\Plugin::class );
+		$plugin->shouldReceive( 'get' )->andReturnUsing(
+			function () {
+				$args = func_get_args();
+				return $args[1] ?? $args[0];
+			}
+		);
+		$container->set( 'plugin', $plugin );
+
+		/**
+		 * @see WP_Http::request()
+		 */
+		add_filter(
+			'pre_http_request',
+			function () {
+				return array(
+					'headers'       => array(),
+					'body'          => '',
+					'response'      => array(
+						'code'    => false,
+						'message' => false,
+					),
+					'cookies'       => array(),
+					'http_response' => null,
+				);
+			}
+		);
+
+		$result = $sut->notify( array( $event ), false );
+
+		$this->assertFalse($result['response']['code']);
+	}
+
+	/**
+	 * @covers ::notify
+	 */
 	public function test_notify_wp_error(): void {
 
 		$sut = new HiiveConnection();
