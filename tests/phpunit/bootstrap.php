fb72ba7bcefea9e63a0f2a603f548144c802b2ce
Brian Henry <BrianHenryIE@gmail.com>
2024-07-08 13:00:07 -0700

    Add `forceWpMockStrictModeOn()`/`forceWpMockStrictModeOff()`

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -19,3 +19,23 @@ WP_Mock::activateStrictMode();
  */
 WP_Mock::setUsePatchwork(true);
 WP_Mock::bootstrap();
+
+/**
+ * Add functions so strict mode can be disabled on individual tests.
+ */
+/**
+ * @see \FeatureContext::forceStrictModeOn();
+ */
+function forceWpMockStrictModeOn() {
+	$property = new \ReflectionProperty( 'WP_Mock', '__strict_mode' );
+	$property->setAccessible( true );
+	$property->setValue( true );
+}
+/**
+ * @see \FeatureContext::forceStrictModeOff();
+ */
+function forceWpMockStrictModeOff() {
+	$property = new \ReflectionProperty( 'WP_Mock', '__strict_mode' );
+	$property->setAccessible( true );
+	$property->setValue( false );
+}
