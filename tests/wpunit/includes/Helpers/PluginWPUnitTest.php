0363f528ddf198f8aca038cd7103664addc6cec4
Brian Henry <BrianHenryIE@gmail.com>
2024-11-15 14:55:13 -0800

    Add tests for `NewfoldLabs\WP\Module\Data\Helpers\Plugin`

--- tests/wpunit/includes/Helpers/PluginWPUnitTest.php
+++ tests/wpunit/includes/Helpers/PluginWPUnitTest.php
@@ -65,4 +65,52 @@ class PluginWPUnitTest extends \lucatume\WPBrowser\TestCase\WPTestCase {
 
 		$this->assertGreaterThan(2, count($result));
 	}
+
+	/**
+	 * @see wp_ajax_toggle_auto_updates()
+	 */
+	protected function set_autoupdate(string $basename, bool $enabled): void {
+
+		$state = $enabled ? 'enable' : 'disable';
+
+		$auto_updates = (array) get_site_option( 'auto_update_plugins', array() );
+		$all_plugins = get_plugins();
+
+		if ( 'disable' === $state ) {
+			$auto_updates = array_diff( $auto_updates, array( $basename ) );
+		} else {
+			$auto_updates[] = $basename;
+			$auto_updates   = array_unique( $auto_updates );
+		}
+
+		$auto_updates = array_intersect( $auto_updates, array_keys( $all_plugins ) );
+
+		update_site_option( 'auto_update_plugins', $auto_updates );
+	}
+
+	/**
+	 * @covers ::does_it_autoupdate
+	 */
+	public function test_does_it_autoupdate(): void {
+		$sut = new Plugin();
+
+		$this->set_autoupdate('bluehost-wordpress-plugin/bluehost-wordpress-plugin.php', true);
+		$this->set_autoupdate('jetpack/jetpack.php', false);
+
+		$this->assertTrue($sut->collect('bluehost-wordpress-plugin/bluehost-wordpress-plugin.php')['auto_updates']);
+		$this->assertFalse($sut->collect('jetpack/jetpack.php')['auto_updates']);
+	}
+
+	/**
+	 * @covers ::does_it_autoupdate
+	 */
+	public function test_does_it_autoupdate_all(): void {
+		$sut = new Plugin();
+
+		add_site_option('auto_update_plugin', 'false');
+
+		$this->set_autoupdate('bluehost-wordpress-plugin/bluehost-wordpress-plugin.php', false);
+
+		$this->assertTrue($sut->collect('bluehost-wordpress-plugin/bluehost-wordpress-plugin.php')['auto_updates']);
+	}
 }
