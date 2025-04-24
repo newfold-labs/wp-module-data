34f0eb27d05c85e6db83efe618bfdfb9a1446c7e
Brian Henry <BrianHenryIE@gmail.com>
2025-04-24 15:32:28 -0700

    Remove manual activation of plugin

--- tests/integration/_bootstrap.php
+++ tests/integration/_bootstrap.php
@@ -2,6 +2,3 @@
 /**
  * Runs after WordPress has been initialised (after plugins are loaded) and before tests are run.
  */
-
-activate_plugin( 'wp-module-data/wp-module-data-plugin.php' );
-require codecept_root_dir( 'bootstrap.php' );
