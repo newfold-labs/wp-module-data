a5a09ee206310a899a6a2db50ffb3eb1106f5448
Brian Henry <BrianHenryIE@gmail.com>
2025-04-24 15:16:09 -0700

    Activate the plugin and explicitly load `bootstrap.php`

--- tests/integration/_bootstrap.php
+++ tests/integration/_bootstrap.php
@@ -3,3 +3,5 @@
  * Runs after WordPress has been initialised (after plugins are loaded) and before tests are run.
  */
 
+activate_plugin( 'wp-module-data/wp-module-data-plugin.php' );
+require codecept_root_dir( 'bootstrap.php' );
